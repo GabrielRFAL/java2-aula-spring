ed7fd208974ce741466b9e832f45f659d97abe88
Gabriel Ribas Ferreira de Almeida <165100529+GabrielRFAL@users.noreply.github.com>
2025-08-29 17:52:40 +0000

    alteracoes tarefaController

--- src/main/java/application/controller/TarefaController.java
+++ src/main/java/application/controller/TarefaController.java
@@ -1,13 +1,25 @@
 package application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.model.Tarefa;
 
 @Controller
 @RequestMapping("/tarefas")
 public class TarefaController {
+    private List<Tarefa> tarefas = new ArrayList<Tarefa>();
+
     @RequestMapping("/list")
-    public String list() {
+    public String list(Model ui) {
+        tarefas.add(new Tarefa(1, "Aprender Java"));
+        tarefas.add(new Tarefa(2, "Estudar IoT"));
+
+        ui.addAttribute("tarefas", tarefas);
         return "list";
     }
 }
